3efc3eac56bf6175ebede51efd4f61d50a70820b
paccy-tech <pacifiquetwagirayesu@gmail.com>
2020-05-05 11:37:47 +0800

    initial commit

---
+++ src/org/mine/App.java
@@ -0,0 +1,16 @@
+package org.mine;
+
+
+	public class App {
+
+		public static void main(String[] args) {
+			
+			int num = 9;
+			int num2 = 10;
+			
+			System.out.println(num + num2);
+		}
+
+	}
+
+
